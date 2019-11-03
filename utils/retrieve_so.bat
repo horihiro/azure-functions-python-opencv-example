ad0c01026733480bce325147f53d59c207efadad
horihiro <hori.kawa.shima.tool+github@gmail.com>
2019-11-03 09:21:43 +0900

    separate pkg/lib name from scripts

--- utils/retrieve_so.bat
+++ utils/retrieve_so.bat
@@ -1,5 +1,15 @@
 @echo off
+echo | set /p="Preparing container... "
 for /f "usebackq" %%A in (`docker run -d --rm -it mcr.microsoft.com/azure-functions/python /bin/bash`) do set CID=%%A
-docker exec %CID% sh -c "apt update && apt install libglib2.0-0 -y && cp -L $(ldconfig -p | grep -E 'libg(thread|lib)-2.0.so.0' | sed -E 's/.* ([^ ]+$)/\1/') /tmp/"
-docker cp %CID%:/tmp/ ./lib
-docker kill %CID%
+docker cp ./utils/ %CID%:/tmp/ > nul 2>&1
+echo done
+echo | set /p="Installing necessary libraries into container... "
+docker exec %CID% sh -c "mkdir -p /tmp/lib && apt update && apt install $(cat /tmp/utils/additional_packages.txt | grep -v \"^#\" | tr \"\n\" \" \") -y && cp -L $(ldconfig -p | grep -E \"($(cat /tmp/utils/necessary_libraries.txt ^| grep -v '^#' ^| tr '\n' '^|' ^| sed -E 's/\^|{1,}$//'))\" | sed -E 's/.* ([^ ]+$)/\1/') /tmp/lib" > nul 2>&1
+echo done
+echo | set /p="Copy libraries from container... "
+docker cp %CID%:/tmp/lib/ ./lib > nul 2>&1
+echo done
+echo | set /p="Shuting down container... "
+docker kill %CID% > nul 2>&1
+echo done
+echo Finished.
--- utils/retrieve_so.sh
+++ utils/retrieve_so.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+echo -n "Preparing container... "
 CID=$(docker run -d --rm -it mcr.microsoft.com/azure-functions/python /bin/bash)
-docker exec ${CID} sh -c \
-  'apt update && apt install libglib2.0-0 -y && cp -L $(ldconfig -p | grep -E "libg(thread|lib)-2.0.so.0" | sed -E "s/.* ([^ ]+$)/\1/") /tmp/'
-docker cp ${CID}:/tmp/ ./lib
-docker kill ${CID}
+docker cp ./utils/ ${CID}:/tmp/ > /dev/null 2>&1
+echo "done"
+echo -n "Installing necessary libraries into container... "
+docker exec ${CID} sh -c 'mkdir -p /tmp/lib && apt update && apt install $(cat /tmp/utils/additional_packages.txt | grep -v "^#" | tr "\n" " ") -y && cp -L $(ldconfig -p | grep -E "($(cat /tmp/utils/necessary_libraries.txt | grep -v "^#" | tr "\n" "|" | sed -E "s/\|{1,}$//"))" | sed -E "s/.* ([^ ]+$)/\1/") /tmp/lib' > /dev/null 2>&1
+echo "done"
+echo -n "Copy libraries from container... "
+docker cp ${CID}:/tmp/lib . > /dev/null 2>&1
+echo "done"
+echo -n "Shuting down container... "
+docker kill ${CID} > /dev/null 2>&1
+echo "done"
+echo "Finished."
